7c460fc629deec55a8f7c9905f035c200873b484
Acirazusaa <magistradozach@gmail.com>
2025-02-21 08:14:50 +0800

    new branch controllers done

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -15,7 +15,7 @@ const getWorkouts = async (req, res) => {
 const getWorkout = async (req, res) => {
     const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) {
+    if (!mongoose.Types.ObjectId.isValid(id)) { 
         return res.status(404).json({ error: 'Invalid workout ID' });
     }
 
@@ -44,8 +44,45 @@ const createWorkout = async (req, res) => {
     }
 };
 
+//delete workout
+const deleteWorkout = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) { 
+        return res.status(404).json({ error: 'Invalid workout ID' });
+    }
+    
+    const workout = await Workout.findOneAndDelete({_id: id})
+
+    if (!workout) {
+        return res.status(400).json({ error: 'No such workout' });
+    }
+    
+    res.status(200).json(workout)
+};
+
+//update workout
+const updateWorkout = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) { 
+        return res.status(404).json({ error: 'Invalid workout ID' });
+    }
+
+    const workout = await Workout.findOneAndUpdate({_id: id},{
+        ...req.body
+    })
+
+    if (!workout) {
+        return res.status(400).json({ error: 'No such workout' });
+    }
+    res.status(200).json(workout)
+}
+
 module.exports = {
     getWorkouts,
     getWorkout,
-    createWorkout
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
 };
