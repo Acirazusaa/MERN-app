9d9ea86d1f45b58873373897e5ae86e47c38e0bc
Acirazusaa <magistradozach@gmail.com>
2025-02-24 15:03:12 +0800

    Lesson #10 - Create new workout form

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,35 @@
-const Home = () => {
-    return(
-        <div className="home">
-            <h2>Home</h2>
-        </div>
-    )
-}
+import { useEffect, useState } from 'react';
 
-export default Home
+//components
+import WorkoutDetail from '../components/WorkoutDetail';
+import WorkoutForm from '../components/WorkoutForm';
+
+const Home = () => {
+    const [workouts, setWorkouts] = useState(null);
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts');
+            const json = await response.json();
+
+            if (response.ok) {
+                setWorkouts(json);
+            }
+        };
+
+        fetchWorkouts();
+    }, []);
+
+    return (
+        <div className="home">
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetail key={workout._id} workout={workout} />
+                ))}
+            </div>
+            <WorkoutForm/>
+        </div>
+    );
+};
+
+export default Home;
